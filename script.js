259e905a0e816094104a31f09970fcb7301a92eb
Goldy <goldy0316.07@gmail.com>
2024-07-01 00:32:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('E_classSelect').addEventListener('change', function() {
+    var selectedValue = this.value;
+    var percentage;
+
+    if (selectedValue === '12th') {
+        percentage = '56.6%';
+    } else if (selectedValue === '10th') {
+        percentage = '61.8%';
+    }
+
+    var infoDiv = document.getElementById('percentageInfo');
+    infoDiv.innerHTML = 'Percentage: ' + percentage;
+    infoDiv.style.display = 'block'; // Show the info div
+}
+);
